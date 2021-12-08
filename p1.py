034466014e75a9b26ef75e1b767c7f27743697b7
Daniel Tang <Daniel.Tang@wnco.com>
2021-12-08 15:05:24 -0600

    part 2 done

--- p1.py
+++ p1.py
@@ -29,10 +29,22 @@ def compare_and_count(int_arr):
     int_arr_tuples = zip(int_arr, int_arr[1:])
     return sum(map(lambda x: 1 if x[0] < x[1] else 0, int_arr_tuples))
 
+def compare_and_count_sliding_window(int_arr):
+    """
+    Sums adjacent triples in array and delegates to compare_and_count to find the number 
+    of increases in the sums
+
+    int_arr - array of integers
+    returns number of instances where the number of times a triplet sum increases
+    """
+    sum_arr_triples = list(map(lambda x: sum(x), zip(int_arr, int_arr[1:], int_arr[2:])))
+    return compare_and_count(sum_arr_triples)
+
 def main():
-    fName = "p1_input.txt"
+    fName = "./p1_input.txt"
     input = array_string_2_int(readFile("p1_input.txt"))
     print("Answer to Part 1 of Day 1: " + str(compare_and_count(input)))
+    print("Answer to Part 2 of Day 1: " + str(compare_and_count_sliding_window(input)))
 
 if __name__ == "__main__":
     main()
